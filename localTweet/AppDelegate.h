49a0e48f4d98ccacaa785f2970a222b4c43a02d3
zuzu <clown.boy.zuzu@gmail.com>
2012-05-17 10:07:18 +0900

    Initial Commit

---
+++ localTweet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  localTweet
+//
+//  Created by zuzu on 12/05/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ localTweet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  localTweet
+//
+//  Created by zuzu on 12/05/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
